0cf056d910a83be4528ed81284de0459e6d912bb
joeyaza <JAzadehdel@gmail.com>
2022-09-29 11:51:02 +0200

    feat: deep nesting works with recur and reduce, tests pass

--- flattenArray.js
+++ flattenArray.js
@@ -1,18 +1,28 @@
 const flattenArray = (arr) => {
 
-    if (isNotArray(arr)) {
+    if (!isArray(arr)) {
 
         throw new Error("Input is not an array");
 
     }
 
-    return [].concat(...arr);
+    return flatten(arr);
 
 }
 
-const isNotArray = (arr) => {
+const flatten = (arr) => {
 
-    return !Array.isArray(arr);
+    return arr.reduce((accum, el) => {
+
+        return accum.concat(isArray(el) ? flatten(el) : el);
+
+      }, []);
+
+}
+
+const isArray = (arr) => {
+
+    return Array.isArray(arr);
 
 }
 
